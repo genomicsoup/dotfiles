2ce18586def92312257d5f8265db37f729fb9b0f
genomicsoup <genomicsoup>
2022-10-13 21:03:24 -0500

    Initial commit

---
+++ .config/fish/config.fish
@@ -0,0 +1,26 @@
+
+# directory colors
+eval (gdircolors -c $HOME/.dir_colors)
+
+function fish_user_key_bindings
+    bind \cr 'peco_select_history (commandline -b)'
+end
+direnv hook fish | source
+
+# Path variables
+set PATH $HOME/.local/bin $PATH
+set PATH $HOME/edirect $PATH
+set PATH $HOME/.cargo/bin $PATH
+set PATH $HOME/.local/bin/ncbi $PATH
+
+# Misc env variables
+# stop homebrew from fucking updating and breaking everything during brew install
+set -gx HOMEBREW_NO_AUTO_UPDATE 1
+
+# set up ssh-agent
+fish_ssh_agent
+
+# aliases
+alias vim "nvim"
+alias ls "gls --color=auto"
+alias dotfiles "git --git-dir=$HOME/.dotfiles --work-tree=$HOME"
