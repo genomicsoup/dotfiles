b6f1965c90e606e3e1905ca376c0fe43221b0ca0
genomicsoup <genomicsoup>
2022-10-13 22:55:04 -0500

    Attempt to fix pyenv paths

--- .config/fish/conf.d/pyenv.fish
+++ .config/fish/conf.d/pyenv.fish
@@ -1,5 +1,7 @@
 set --export PYENV_ROOT $HOME/.pyenv
 
+fish_add_path $PYENV_ROOT/bin
+
 # fish_add_path "$PYENV_ROOT/shims"
 contains "$PYENV_ROOT/shims" $fish_user_paths; or set -Ua fish_user_paths "$PYENV_ROOT/shims"
 
