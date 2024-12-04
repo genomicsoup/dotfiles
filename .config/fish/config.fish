0f6d3e72edf0eeada1ead4976571034a33827e98
genomicsoup <95888891+genomicsoup@users.noreply.github.com>
2024-12-03 18:24:13 -0600

    initial set up for fish vi mode

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,9 +2,6 @@
 # directory colors
 eval (dircolors -c $HOME/.dir_colors)
 
-function fish_user_key_bindings
-    bind \cr 'peco_select_history (commandline -b)'
-end
 direnv hook fish | source
 
 # Path variables
@@ -16,6 +13,19 @@ set PATH $HOME/.local/bin/ncbi $PATH
 # Misc env variables
 # stop homebrew from fucking updating and breaking everything during brew install
 set -gx HOMEBREW_NO_AUTO_UPDATE 1
+
+# vi mode
+#set -g fish_key_bindings fish_vi_key_bindings
+
+function fish_user_key_bindings
+    bind \cr 'peco_select_history (commandline -b)'
+    bind -M insert \cf accept-autosuggestion
+    bind -M default _ beginning-of-line
+
+    fish_vi_key_bindings
+end
+
+set -g fish_key_bindings fish_user_key_bindings
 
 # set up ssh-agent
 fish_ssh_agent
